2748f137007be4a317de249c2fe6c778c8f0afe9
Andrew Stillman <astillman@gmail.com>
2016-02-14 22:42:20 -0500

    Fixes to part 2

--- Hw 1/Part 2/index.js
+++ Hw 1/Part 2/index.js
@@ -2,14 +2,13 @@ angular.module('MyApp', [])
 .controller('MainController', function() {
 	var self = this; 
 	self.folders = [
-	  {name: 'Folder 1', files: ['File 1.1', 'File 1.2', 'File 1.3']},
-	  {name: 'Folder 2', files: ['File 2.1', 'File 2.2', 'File 2.3']},
-	  {name: 'Folder 3', files: ['File 3.1', 'File 3.2', 'File 3.3']}
+	  {name: 'Folder 1', files: [{name: 'File 1.1'},{name: 'File 1.2'}, {name: 'File 1.3'}]},
+	  {name: 'Folder 2', files: [{name: 'File 2.1'},{name: 'File 2.2'}, {name: 'File 2.3'}]},
+	  {name: 'Folder 3', files: [{name: 'File 3.1'},{name: 'File 3.2'}, {name: 'File 3.3'}]}
 	];
 	self.expanded = true;
 	self.onButtonClick = function() {
 		//alert('clicked');
-		self.folders[self.selectedFolderIndex].files.push(self.toAdd);
-		console.log('test');
+		self.folders[self.selectedFolderIndex].files.push({name: self.toAdd});
 	};
 });
