786fa47412d8c2761e31e24e9fd48da54f145745
Andrew Stillman <astillman@gmail.com>
2016-02-14 22:12:32 -0500

    Fixes to part 1

--- Hw 1/Part 1/index.js
+++ Hw 1/Part 1/index.js
@@ -1,10 +1,21 @@
 angular.module('MyApp', [])
-.controller('MainController', function() {
-	var self = this; 
-	self.classObject = {
-		default: true,
-		yellow: false,
-		green: false,
-		red: false
-	}
-});
+	.controller('MainController', function() {
+		var self = this; 
+		self.theme = "default";
+		self.classObject = {
+			default: true,
+			yellow: false,
+			green: false,
+			red: false,
+			blue: false
+		};
+		self.updateTheme = function() {
+			for (var key in self.classObject) {
+				if (self.theme === key) {
+					self.classObject[key] = true;
+				} else {
+					self.classObject[key] = false;
+				}
+			}
+		};
+	});
